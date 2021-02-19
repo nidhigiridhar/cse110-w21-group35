a7a96fdf2325bd2dc99b86e0ad7de52cd3b67fb4
sywong888 <56745380+sywong888@users.noreply.github.com>
2021-02-18 16:31:22 -0800

    test doc action on button

--- source/buttons.js
+++
@@ -1,4 +0,0 @@
-// startButton calls onStart() when clicked
-document.getElementById("startButton").addEventListener("click", onStart);
-// resetButton calls onReset() when clicked
-document.getElementById("resetButton").addEventListener("click", onReset);
--- source/timer.js
+++ source/timer.js
@@ -91,3 +91,8 @@ function checkState(){
     }
 
 }
+
+// startButton calls onStart() when clicked
+document.getElementById("startButton").addEventListener("click", onStart);
+// resetButton calls onReset() when clicked
+document.getElementById("resetButton").addEventListener("click", onReset);
