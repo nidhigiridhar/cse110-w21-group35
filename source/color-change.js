cdab6f510c45a204ff0ed5d4b13438295697b6ee
Jazmusic1234 <jcc001@ucsd.edu>
2021-02-23 15:54:15 -0800

    Added on change listener

--- source/color-change.js
+++ source/color-change.js
@@ -1,9 +1,16 @@
-import { timer } from './timer.js';
+import { timer, WORK_STATE, SHORT_STATE, LONG-STATE } from './timer.js';
+
+let state = timer.currState;
+state.addEventListener("change", colorChange());
 
 /**
  * 
  * 
  */
 function colorChange() {
+  switch(state) {
+    case WORK_STATE:
 
+    case SHORT_STATE
+  }
 }
