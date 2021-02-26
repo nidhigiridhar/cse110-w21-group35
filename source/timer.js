8c2b9fa5fabec4fe2de87006c1c9e1a1b86df9af
NidhiGiridhar <nidhigiridhar@gmail.com>
2021-02-25 16:11:37 -0800

    removed idle mode

--- source/timer.js
+++ source/timer.js
@@ -129,7 +129,7 @@ function updateTimer(duration) {
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        console.log(minutes + ':' + seconds);
+        //console.log(minutes + ':' + seconds);
         document.getElementById("timer-display").innerHTML = 
             `${minutes}:${seconds}`;
 
