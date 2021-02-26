5d288dd3a6a39bf1c0aeac3c1ed58a97dcc9d3f3
Alex-942 <a1marque@ucsd.edu>
2021-02-25 23:56:56 -0800

    Refactored Old Tests, Made tests for add. Features

--- cypress/integration/Productoro-Tests/UI-tests.js
+++ cypress/integration/Productoro-Tests/UI-tests.js
@@ -1,6 +1,7 @@
+//Inital No Actvity Tests
 describe('Fresh Entry, No Activity Tests', () => {
   beforeEach(() => {
-    cy.visit('http://127.0.0.1:5500/source/productoro.html');
+    cy.visit('https://nidhigiridhar.github.io/cse110-w21-group35/source/productoro.html');
   });
 
   it('Timer at 25 minutes', () => {
@@ -14,14 +15,13 @@ describe('Fresh Entry, No Activity Tests', () => {
   });
 
   it('Reset Button not avialable', () => {
-    //Currently does not work ... I think we should disable reset whenever timer not running ... including at start
     cy.get('#resetButton').then(($el) => {
-      //expect($el).to.have.attr('disabled');
+      expect($el).to.have.attr('disabled');
     })
   });
 
   it('Initial State', () => {
-    cy.get('#state').should('have.text','Idle Mode');
+    cy.get('#state').should('have.text','Work State');
   });
 
   it('Initial Pomo Streak', () => {
@@ -31,8 +31,24 @@ describe('Fresh Entry, No Activity Tests', () => {
   it('Initial Pomo Total', () => {
     cy.get('#total').should('have.text','0');
   });
+
+  it('Help Not displayed', () => {
+    cy.get('#help-modal').then(($el) => {
+      expect($el).to.be.hidden
+    });
+  });
+
+  it('Background Color: Blue', () => {
+    cy.get('body').then(($el) => {
+      expect($el).to.have.attr('state', 'pomo');
+    });
+  });
 });
 
+
+
+
+//Start Button Tests
 describe('Start Button Tests', () => {
     beforeEach(() => {
       cy.visit('http://127.0.0.1:5500/source/productoro.html');
@@ -72,10 +88,142 @@ describe('Start Button Tests', () => {
     it('Start Button Clicked: Check State', () => {
       cy.get('#startButton').click();
       //States need to be more consistent ... sometimes they have mode as a suffix sometimes not
-      cy.get('#state').should('have.text','Work');
+      cy.get('#state').should('have.text','Work State');
+    });
+
+    it('Start Button Clicked: Help Not displayed', () => {
+      cy.get('#startButton').click();
+      cy.get('#help-modal').then(($el) => {
+        expect($el).to.be.hidden
+      });
+    });
+  
+    it('Start Button: Clickd Background Color Still Blue', () => {
+      cy.get('#startButton').click();
+      cy.get('body').then(($el) => {
+        expect($el).to.have.attr('state', 'pomo');
+      });
     });
 
 });
+
+
+
+
+
+
+describe('Reset Button Tests', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:5500/source/productoro.html');
+  });
+
+  it('Reset Button Clicked: Timer Resets', () => {
+    cy.get('#startButton').click();
+    //Cypress will wait a 10 seconds after the click
+    cy.wait(10000);
+    cy.get('#timer-display').should('have.text','24:50');
+    cy.get('#resetButton').click();
+    cy.get('#timer-display').should('have.text','25:00');
+  });
+
+  it('Reset Button Clicked: Check Reset Button Gets Disabled', () => {
+    cy.get('#startButton').click();
+    cy.get('#resetButton').click();
+    cy.get('#resetButton').then(($el) => {
+      expect($el).to.have.attr('disabled');
+    })
+  });
+
+  it('Reset Button Clicked: Check Start Button Gets Enabled', () => {
+    cy.get('#startButton').click();
+    cy.get('#resetButton').click();
+    cy.get('#startButton').then(($el) => {
+      expect($el).to.not.have.attr('disabled');
+    })
+  });
+
+  it('Reset Button Clicked: Check if Only Streak was Killed', () => {
+    cy.get('#startButton').click();
+    //Cypress will wait 5 seconds after the click
+    cy.wait(5000)
+    //Not sure if this is the right way to set the inner html
+    cy.get('#streak').invoke('prop', 'innerHTML', '1');
+    cy.get('#total').invoke('prop', 'innerHTML', '1');
+    cy.get('#streak').should('have.text','1');
+    cy.get('#total').should('have.text','1');
+    cy.wait(5000);
+    //reset broken this test will fail
+    cy.get('#resetButton').click();
+    cy.get('#streak').should('have.text','0');
+  });
+
+  it('Reset Button Clicked: Check State', () => {
+    cy.get('#startButton').click();
+    cy.get('#resetButton').click();
+    cy.get('#state').should('have.text','Work State');
+  });
+
+  it('Help Not displayed', () => {
+    cy.get('#help-modal').then(($el) => {
+      expect($el).to.be.hidden
+    });
+  });
+
+  it('Background Color: Blue', () => {
+    cy.get('body').then(($el) => {
+      expect($el).to.have.attr('state', 'pomo');
+    });
+  });
+});
+
+
+
+
+
+//Not Sure how to test TBH
+describe('Help Button Tests', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:5500/source/productoro.html');
+  });
+
+  it('Help Butto Clicked: Instructions Appear', () => {
+    cy.get('#helpButton').click();
+    cy.get('#help-modal').then(($el) => {
+      expect($el).to.be.not.hidden
+    });
+  });
+
+  it('Help Butto Clicked: Timer Does not Start', () => {
+    cy.get('#helpButton').click();
+    cy.get('#help-modal').then(($el) => {
+      expect($el).to.be.not.hidden
+    });
+    cy.get('#timer-display').should('have.text','25:00');
+  });
+
+  it('Help Butto Clicked: Counters Unaffected', () => {
+  });
+
+});
+
+//Not Sure how to test TBH
+describe('Dynamic Background Tests', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:5500/source/productoro.html');
+  });
+
+  it('Color Chnages: Short Break', () => {
+  });
+
+  it('Color Chnages: Long Break', () => {
+  });
+
+});
+
+
+
+
+
 
 describe('Entire Pomo Test', () => {
   beforeEach(() => {
@@ -85,12 +233,12 @@ describe('Entire Pomo Test', () => {
   //May or may not work ... ONLY TIME WILL TELL ... LITERALLY
   it('Start Button Clicked: Check Timer Display', () => {
     cy.get('#startButton').click();
-    cy.get('#state').should('have.text','Work');
+    cy.get('#state').should('have.text','Work State');
     //Cypress will wait entire pomo
     cy.wait(25*60*1000);
     //check if timer is updated correctly
-    cy.get('#timer-display').should('have.text','0:00');
-    cy.get('#timer-display').should('have.text','5:00');
+    cy.get('#timer-display').should('have.text','00:00');
+    cy.get('#timer-display').should('have.text','05:00');
     //Check if the buttons flip press-ibility
     cy.get('#startButton').then(($el) => {
       expect($el).to.not.have.attr('disabled');
@@ -99,20 +247,28 @@ describe('Entire Pomo Test', () => {
       expect($el).to.have.attr('disabled');
     })
     //check state switches
-    cy.get('#state').should('have.text','Short Break');
+    cy.get('#state').should('have.text','Short Break State');
     //check counters get updated
     cy.get('#streak').should('have.text','1');
     cy.get('#total').should('have.text','1');
 
-    //pass through short break
+    //start short break
     cy.get('#startButton').click();
+    //check both buttons cannot be pressed during break
+    cy.get('#startButton').then(($el) => {
+      expect($el).to.have.attr('disabled');
+    })
+    cy.get('#resetButton').then(($el) => {
+      expect($el).to.have.attr('disabled');
+    })
     cy.wait(1000*60*5);
-    cy.get('#state').should('have.text','Work');
+    cy.get('#state').should('have.text','Work State');
     //check if pressing reset after short break will correctly update counter, btns, time, etc.
+    cy.wait(5000);
     cy.get('#startButton').click();
     cy.wait(5000);
     cy.get('#resetButton').click();
-    cy.get('#state').should('have.text','Work');
+    cy.get('#state').should('have.text','Work State');
     cy.get('#streak').should('have.text','0');
     cy.get('#total').should('have.text','1');
     cy.get('#timer-display').should('have.text','25:00');
@@ -124,4 +280,13 @@ describe('Entire Pomo Test', () => {
     })
   });
 
+});
+
+describe('Full Cycle Test', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:5500/source/productoro.html');
+  });
+
+  it('Testing Four Pomos Along with Short and Long Breaks', () => {
+  });
 });
