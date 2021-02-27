21e54d1c3aa327bb3606b8967ed8ac4907819bbe
Alex-942 <a1marque@ucsd.edu>
2021-02-27 01:35:20 -0800

    Fully working 30 minute Pomo tests

--- cypress/integration/Productoro-Tests/UI-tests.js
+++ cypress/integration/Productoro-Tests/UI-tests.js
@@ -54,7 +54,7 @@ describe('Start Button Tests', () => {
       cy.visit('https://nidhigiridhar.github.io/cse110-w21-group35/source/productoro.html');
     });
 
-    it('Start Button Clicked: Check Timer Display', () => {
+    it('Start Button Clicked: Check Timer Display 24:50', () => {
       cy.get('#startButton').click();
       //Cypress will wait a 10 seconds after the click
       cy.wait(10000);
@@ -91,14 +91,14 @@ describe('Start Button Tests', () => {
       cy.get('#state').should('have.text','Work State');
     });
 
-    it('Start Button Clicked: Help Not displayed', () => {
+    it('Start Button Clicked: Check Help Not displayed', () => {
       cy.get('#startButton').click();
       cy.get('#help-modal').then(($el) => {
         expect($el).to.be.hidden
       });
     });
   
-    it('Start Button: Clickd Background Color Still Blue', () => {
+    it('Start Button Clickd: Check Background Color Unaffected', () => {
       cy.get('#startButton').click();
       cy.get('body').then(($el) => {
         expect($el).to.have.attr('state', 'pomo');
@@ -139,10 +139,10 @@ describe('Reset Button Tests', () => {
     cy.get('#resetButton').click();
     cy.get('#startButton').then(($el) => {
       expect($el).to.not.have.attr('disabled');
-    })
+    });
   });
 
-  it('Reset Button Clicked: Check if Only Streak was Killed', () => {
+  it('Reset Button Clicked: Check Only Streak was Killed', () => {
     cy.get('#startButton').click();
     //Cypress will wait 5 seconds after the click
     cy.wait(5000)
@@ -163,13 +163,13 @@ describe('Reset Button Tests', () => {
     cy.get('#state').should('have.text','Work State');
   });
 
-  it('Help Not displayed', () => {
+  it('Reset Button Clicked: Help Not displayed', () => {
     cy.get('#help-modal').then(($el) => {
       expect($el).to.be.hidden
     });
   });
 
-  it('Background Color: Blue', () => {
+  it('Reset Button Clicked: Check Background Color Unaffected', () => {
     cy.get('body').then(($el) => {
       expect($el).to.have.attr('state', 'pomo');
     });
@@ -186,14 +186,14 @@ describe('Help Button Tests', () => {
     cy.visit('https://nidhigiridhar.github.io/cse110-w21-group35/source/productoro.html');
   });
 
-  it('Help Butto Clicked: Instructions Appear', () => {
+  it('Help Button Clicked: Instructions Appear', () => {
     cy.get('#helpButton').click();
     cy.get('#help-modal').then(($el) => {
       expect($el).to.be.not.hidden
     });
   });
 
-  it('Help Butto Clicked: Timer Does not Start', () => {
+  it('Help Button Clicked: Timer Does not Start', () => {
     cy.get('#helpButton').click();
     cy.get('#help-modal').then(($el) => {
       expect($el).to.be.not.hidden
@@ -201,9 +201,52 @@ describe('Help Button Tests', () => {
     cy.get('#timer-display').should('have.text','25:00');
   });
 
-  it('Help Butto Clicked: Counters Unaffected', () => {
+  it('Help Button Clicked: Start Button Unaffected', () => {
+    cy.get('#helpButton').click();
+    cy.get('#help-modal').then(($el) => {
+      expect($el).to.be.not.hidden
+    });
+    cy.get('#startButton').then(($el) => {
+      expect($el).to.not.have.attr('disabled');
+    });
   });
 
+  it('Help Button Clicked: Reset Button Unaffected', () => {
+    cy.get('#helpButton').click();
+    cy.get('#help-modal').then(($el) => {
+      expect($el).to.be.not.hidden
+    });
+    cy.get('#resetButton').then(($el) => {
+      expect($el).to.have.attr('disabled');
+    });
+  });
+
+  it('Help Button Clicked: Background Color Unaffected', () => {
+    cy.get('#helpButton').click();
+    cy.get('#help-modal').then(($el) => {
+      expect($el).to.be.not.hidden
+    });
+    cy.get('body').then(($el) => {
+      expect($el).to.have.attr('state', 'pomo');
+    });
+  });
+
+  it('Help Button Clicked: Current State Unaffected', () => {
+    cy.get('#helpButton').click();
+    cy.get('#help-modal').then(($el) => {
+      expect($el).to.be.not.hidden
+    });
+    cy.get('#state').should('have.text','Work State');
+  });
+
+  it('Help Button Clicked: Counters Unaffected', () => {
+    cy.get('#helpButton').click();
+    cy.get('#help-modal').then(($el) => {
+      expect($el).to.be.not.hidden
+    });
+    cy.get('#streak').should('have.text','0');
+    cy.get('#total').should('have.text','0');
+  });
 });
 
 //Not Sure how to test TBH
@@ -212,11 +255,14 @@ describe('Dynamic Background Tests', () => {
     cy.visit('https://nidhigiridhar.github.io/cse110-w21-group35/source/productoro.html');
   });
 
-  it('Color Chnages: Short Break', () => {
+  it('Color Changes: Short Break', () => {
   });
 
-  it('Color Chnages: Long Break', () => {
+  it('Color Changes: Long Break', () => {
   });
+
+  it('Color Changes: Long Break', () => {
+  }); 
 
 });
 
@@ -225,61 +271,168 @@ describe('Dynamic Background Tests', () => {
 
 
 
-describe('Entire Pomo Test', () => {
+describe('Full Test', () => {
   beforeEach(() => {
     cy.visit('https://nidhigiridhar.github.io/cse110-w21-group35/source/productoro.html');
   });
 
-  //May or may not work ... ONLY TIME WILL TELL ... LITERALLY
-  it('Start Button Clicked: Check Timer Display', () => {
+  it('Entire Pomo Test', () => {
+    //Start Pomo
     cy.get('#startButton').click();
+    //Check current state is correct
     cy.get('#state').should('have.text','Work State');
+    //Checks that the start button gets disabled
+    cy.get('#startButton').then(($el) => {
+      expect($el).to.have.attr('disabled');
+    });
+    //Checks that the restart button gets enabled
+    cy.get('#resetButton').then(($el) => {
+      expect($el).to.not.have.attr('disabled');
+    });
+    //Check current counters are correct
+    cy.get('#streak').should('have.text','0');
+    cy.get('#total').should('have.text','0');
+    //Check help cannot be seen
+    cy.get('#help-modal').then(($el) => {
+      expect($el).to.be.hidden;
+    });
+    //Check background color is blue
+    cy.get('body').then(($el) => {
+      expect($el).to.have.attr('state', 'pomo');
+    });
+
+
+
     //Cypress will wait entire pomo
     cy.wait(25*60*1000);
+    //Check that the background changes to orange
+    cy.get('body').then(($el) => {
+      expect($el).to.have.attr('state', 'short');
+    });
     //check if timer is updated correctly
-    cy.get('#timer-display').should('have.text','00:00');
-    cy.get('#timer-display').should('have.text','05:00');
+    cy.get('#timer-display').should('have.text','5:00');
     //Check if the buttons flip press-ibility
     cy.get('#startButton').then(($el) => {
       expect($el).to.not.have.attr('disabled');
-    })
+    });
     cy.get('#resetButton').then(($el) => {
       expect($el).to.have.attr('disabled');
-    })
+    });
     //check state switches
     cy.get('#state').should('have.text','Short Break State');
     //check counters get updated
     cy.get('#streak').should('have.text','1');
     cy.get('#total').should('have.text','1');
+    //Check help cannot be seen
+    cy.get('#help-modal').then(($el) => {
+      expect($el).to.be.hidden;
+    });
+    
+    //Before switching to break
+    cy.wait(5*1000);
 
-    //start short break
+    //Start short break
     cy.get('#startButton').click();
-    //check both buttons cannot be pressed during break
+    //Check background color is still orange
+    cy.get('body').then(($el) => {
+      expect($el).to.have.attr('state', 'short');
+    });
+    //Check that the buttons cannot be pressed
     cy.get('#startButton').then(($el) => {
       expect($el).to.have.attr('disabled');
-    })
+    });
     cy.get('#resetButton').then(($el) => {
       expect($el).to.have.attr('disabled');
-    })
-    cy.wait(1000*60*5);
-    cy.get('#state').should('have.text','Work State');
-    //check if pressing reset after short break will correctly update counter, btns, time, etc.
-    cy.wait(5000);
-    cy.get('#startButton').click();
-    cy.wait(5000);
-    cy.get('#resetButton').click();
-    cy.get('#state').should('have.text','Work State');
-    cy.get('#streak').should('have.text','0');
+    });
+    //check state switches
+    cy.get('#state').should('have.text','Short Break State');
+    //check counters get updated
+    cy.get('#streak').should('have.text','1');
     cy.get('#total').should('have.text','1');
+    //Check help cannot be seen
+    cy.get('#help-modal').then(($el) => {
+      expect($el).to.be.hidden;
+    });
+
+    //Progress through break
+    cy.wait(1000*60*5);
+
+    //Break Finished, Check if the state is back to work
+    cy.get('#state').should('have.text','Work State');
+    //Check the timer is back at 25 minutes
     cy.get('#timer-display').should('have.text','25:00');
+    //Check that only start can be pressed
     cy.get('#startButton').then(($el) => {
       expect($el).to.not.have.attr('disabled');
-    })
+    });
     cy.get('#resetButton').then(($el) => {
       expect($el).to.have.attr('disabled');
-    })
-  });
+    });
+    //Check that the Background color is blue again
+    cy.get('body').then(($el) => {
+      expect($el).to.have.attr('state', 'pomo');
+    });
+    //Check that help is hidden
+    cy.get('#help-modal').then(($el) => {
+      expect($el).to.be.hidden;
+    });
+    //Check Counters are correct
+    cy.get('#streak').should('have.text','1');
+    cy.get('#total').should('have.text','1');
+    
 
+    //Start next Pomo
+    cy.get('#startButton').click();
+    cy.wait(5000);
+
+    //Check the timer is back at 24:55 minutes
+    cy.get('#timer-display').should('have.text','24:55');
+    //Check if the state is still at work
+    cy.get('#state').should('have.text','Work State');
+    //Check that only reset can be pressed
+    cy.get('#startButton').then(($el) => {
+      expect($el).to.have.attr('disabled');
+    });
+    cy.get('#resetButton').then(($el) => {
+      expect($el).to.not.have.attr('disabled');
+    });
+    //Check that the Background color is blue still
+    cy.get('body').then(($el) => {
+      expect($el).to.have.attr('state', 'pomo');
+    });
+    //Check that help is hidden
+    cy.get('#help-modal').then(($el) => {
+      expect($el).to.be.hidden;
+    });
+    //Check Counters are correct
+    cy.get('#streak').should('have.text','1');
+    cy.get('#total').should('have.text','1');
+
+    //Hit that reset button
+    cy.get('#resetButton').click();
+
+    //Check current state is correct
+    cy.get('#state').should('have.text','Work State');
+    //Checks that the start button gets enabled
+    cy.get('#startButton').then(($el) => {
+      expect($el).to.not.have.attr('disabled');
+    });
+    //Checks that the restart button gets disabled
+    cy.get('#resetButton').then(($el) => {
+      expect($el).to.have.attr('disabled');
+    });
+    //Check current counters are correct after the reset click
+    cy.get('#streak').should('have.text','0');
+    cy.get('#total').should('have.text','1');
+    //Check help cannot be seen
+    cy.get('#help-modal').then(($el) => {
+      expect($el).to.be.hidden;
+    });
+    //Check background color is blue
+    cy.get('body').then(($el) => {
+      expect($el).to.have.attr('state', 'pomo');
+    });
+  });
 });
 
 describe('Full Cycle Test', () => {
