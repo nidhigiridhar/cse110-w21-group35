87d82b5b64c34640579c3c08afad0fc75299c2e0
Jazmusic1234 <jcc001@ucsd.edu>
2022-05-06 18:59:29 -0700

    Refactored JS into one main file

--- source/main.js
+++ source/main.js
@@ -4,8 +4,8 @@ import { onStart, onReset, checkState, updateState, timer, setCustomTime, keyboa
 import { revealHelp, hideHelp } from './modules/help.js';
 import { showNotif, getNotificationStatus, playSound, getAlarm } from './modules/notifications.js';
 import { colorChange } from './modules/color-change.js';
-import { breakReminders } from './modules/breakReminder.js'
-import { addTaskButton, cancelTask, saveTask, createCustomTaskTag, finishTask } from './modules/task-list.js'
+import { breakReminders } from './modules/break-reminder.js'
+
 
 // Timer
 document.getElementById('formEnabler').addEventListener('change', setCustomTime);
@@ -20,10 +20,5 @@ document.getElementById('helpButton').addEventListener('click', revealHelp);
 document.getElementById('closeModal').addEventListener('click', hideHelp);
 document.getElementById('settingsButton').addEventListener('click', revealSettings);
 document.getElementById('closeSettings').addEventListener('click', hideSettings);
-
-// Task list
-document.getElementById('addTasksButton').addEventListener('click',addTaskButton);
-document.getElementById('saveButton').addEventListener('click',saveTask);
-document.getElementById('cancelButton').addEventListener('click',cancelTask);
 
   
